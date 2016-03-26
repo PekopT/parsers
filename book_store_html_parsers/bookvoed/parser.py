cda3347fee5f0e5d04c0e8832ddb7c78b1cd1640
Mukhtar <dagdahzub@mail.ru>
2016-03-26 08:42:16 +0300

    #PRS-25 Add new error schema for  bookvoed  [Finishes book_store_html_parsers #115511505]

--- book_store_html_parsers/bookvoed/parser.py
+++ book_store_html_parsers/bookvoed/parser.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 import sys
+import traceback
 import json
 import re
 from codecs import getwriter
 from bs4 import BeautifulSoup
+from jsonschema import validate
 
 sout = getwriter("utf-8")(sys.stdout)
 
@@ -20,34 +23,42 @@ class Parser(object):
 
     def parse_html(self, data):
         url = data['url']
+
         html = data['html']
+        # html = open('book1.html','rb')
 
         soup = BeautifulSoup(html, 'html.parser')
-        info_content = soup.find('div', {'id':'aboutTabs_info_content'}).table
+        info_content = soup.find('div', {'id': 'aboutTabs_info_content'}).table
 
         infos = {}
         for tr in info_content:
             td_list = tr.find_all('td')
-            infos[td_list[0].text] = td_list[1].text
+            if u"втор" in td_list[0].text:
+                infos[u"Автор"] = td_list[1].text
+            else:
+                infos[td_list[0].text] = td_list[1].text
 
         name = soup.find('h1')
 
-        name = str(name)
-        author = infos[u'Автор']
         publisher = infos[u'Издательство']
+        name = name.text
+        author = infos[u'Автор']
+
         year = infos[u'Год']
         pages = infos[u'Страниц']
         isbn = infos[u"ISBN"]
         cover = infos[u"Переплёт"]
 
-        stock = u''
+        stock = u'В наличии'
 
-        description_info= soup.find('div', {'id':'aboutTabs_descr_content'})
-        description = description_info.text
+        description_info = soup.find('div', {'id': 'aboutTabs_descr_content'})
+        description = description_info.text.strip()
+        description = description.replace("\\n", "").strip()
 
         price = soup.find('meta', {'itemprop': 'price'})
         price_currency = soup.find('meta', {'itemprop': 'priceCurrency'})
-        price = price.get('content')
+        price = price.get('content').split('.')[0]
+
         price_currency = price_currency.get('content')
         row = {
             "url": url,
@@ -56,9 +67,9 @@ class Parser(object):
             "publisher": publisher,
             "description": description,
             "price": {
-                "currency": price_currency,
+                "currency": u"RUR",
                 "type": "currency",
-                "content": price
+                "content": int(price)
             },
             "availability": stock,
             "year": year,
@@ -67,10 +78,15 @@ class Parser(object):
             "isbn": isbn,
         }
 
+        self.check_validate_schema(row)
         self.rows_data.append(row)
 
-    def check_validate_schema(self):
-        pass
+
+    def check_validate_schema(self, node):
+        f = open('books.schema.json', 'r')
+        schema = json.loads(f.read())
+        validate(node, schema)
+
 
     def close_parser(self):
         sout.write(json.dumps(self.rows_data, ensure_ascii=False))
@@ -79,16 +95,16 @@ class Parser(object):
 def main():
     parser = Parser()
     for line in sys.stdin:
+        data = json.loads(line)
         try:
-            data = json.loads(line)
-            try:
-                parser.parse_html(data)
-            except Exception:
-                pass
-        except Exception:
-            pass
+            parser.parse_html(data)
+        except Exception as e:
+            sys.stderr.write(
+                json.dumps({"url": data["url"], "traceback": traceback.format_exc()}, ensure_ascii=False).encode(
+                    "utf-8") + "\n")
 
     parser.close_parser()
+
 
 if __name__ == '__main__':
     main()
