e0e613eea858c4bf31b4ce7ca98222ca597d641e
Mukhtar <dagdahzub@mail.ru>
2016-03-30 14:22:43 +0300

    #PRS-25 Fixes parser for bookshop  [Finishes  #book_store_html_parsers #115511505]

--- book_store_html_parsers/bookshop/parser.py
+++ book_store_html_parsers/bookshop/parser.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import traceback
 import json
 import re
 from codecs import getwriter
@@ -129,12 +130,8 @@ class Parser(object):
         if pictures:
             row["images"] = pictures
 
-
-        try:
-            self.check_validate_schema(row)
-            self.rows_data.append(row)
-        except Exception as e:
-            print e.message
+        self.check_validate_schema(row)
+        self.rows_data.append(row)
 
     def check_validate_schema(self, node):
         f = open('books.schema.json', 'r')
@@ -148,17 +145,17 @@ class Parser(object):
 def main():
     parser = Parser()
     for line in sys.stdin:
+        data = json.loads(line)
         try:
-            data = json.loads(line)
-            try:
-                parser.parse_html(data)
-            except Exception as e:
-                print str(e)
+            parser.parse_html(data)
         except Exception as e:
-            print str(e)
+            sys.stderr.write(
+                json.dumps({"url": data["url"], "traceback": traceback.format_exc()}, ensure_ascii=False).encode(
+                    "utf-8") + "\n")
 
     parser.close_parser()
 
 
 if __name__ == '__main__':
     main()
+
