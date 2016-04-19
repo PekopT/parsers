4c451944b857758ba522223f5bb77cfbf2d8729b
Mukhtar <dagdahzub@mail.ru>
2016-04-19 14:35:13 +0300

    #PRS-48 [Finishes read_ru #1390330]

--- book_store_html_parsers/read_ru/parser.py
+++ book_store_html_parsers/read_ru/parser.py
@@ -23,11 +23,14 @@ class Parser(object):
 
     def get_images(self, soup):
         images = []
-        image_ul = soup.find('ul', 'book_image__slidebar_itemlist j-slidebar-product-previews__list').findAll('li')
+        image_ul = soup.find('ul', 'book_image__slidebar_itemlist j-slidebar-product-previews__list')
+        if image_ul:
+            image_ul = image_ul.findAll('li')
 
-        for li in image_ul:
-            img_src = li.find('img')
-            images.append(img_src.get('src'))
+        if image_ul:
+            for li in image_ul:
+                img_src = li.find('img')
+                images.append(img_src.get('src'))
 
         return images
 
@@ -38,58 +41,103 @@ class Parser(object):
 
         info = soup.body.div
 
-        isbn = info.find('span', {'itemprop': 'isbn'})
-        isbn = isbn.text.strip()
+        isbn_info = info.find('span', {'itemprop': 'isbn'})
+        if isbn_info:
+            isbn = isbn_info.text.strip()
+        else:
+            isbn = ''
 
         isbn = isbn.replace('\\n', '')
 
-        author = soup.find('div', 'j-book_autors book_autors').find('span')
-        author = author.text.replace('\\n', '').strip()
+        author_info = soup.find('div', 'j-book_autors book_autors')
+        if author_info:
+            author = author_info.find('span')
+            author = author.text.replace('\\n', '').strip()
+        else:
+            author = ''
 
-        price = soup.find('div', 'book_price3__fullprice').find('div', {'itemprop': 'price'})
-        price = price.text.strip()
-        price = self.str_to_int(price)
+        price_info = soup.find('div', 'book_price3__fullprice')
 
-        price_currency = info.find('meta', {'itemprop': 'priceCurrency'})
-        price_currency = price_currency.get('content').strip()
+        if price_info:
+            price = price_info.find('div', {'itemprop': 'price'})
+            price = price.text.strip()
+            price = self.str_to_int(price)
+        else:
+            price = ''
 
         year = info.find('meta', {'itemprop': 'datePublished'})
-        year = year.get('content').strip()
+
+        if year:
+            year = year.get('content').strip()
+        else:
+            year = ''
 
         description = info.find('span', {'itemprop': 'description'})
-        description = description.text.strip()
-        description = self.remove_tags(description)
+        if description:
+            description = description.text.strip()
+            description = self.remove_tags(description)
+        else:
+            description = ''
 
         name = info.find('span', {'itemprop': 'name'})
-        name = name.text.replace('\\n', '').strip()
+        if name:
+            name = name.text.replace('\\n', '').strip()
+        else:
+            name = ''
 
-        pages = soup.find('td', 'pages').find('span', 'book_field')
-        pages = pages.text.replace('\\n', '').strip()
+        pages_td = soup.find('td', 'pages')
 
-        publisher = soup.find('li', 'j-book_pub').find('td', 'pubhouse')
-        publisher = publisher.text.strip()
-        publisher = self.remove_tags(publisher)
-        publisher = publisher.replace('\\n', '').strip()
+        if pages_td:
+            pages = pages_td.find('span', 'book_field')
+            if pages:
+                pages = pages.text.replace('\\n', '').strip()
+            else:
+                pages = ''
+        else:
+            pages = ''
 
-        price_stock = soup.find('div', 'book_price3').find('div', 'book_price__title_ok-thin')
-        price_stock = price_stock.text.replace('\\n', '').strip()
-        price_stock = self.remove_tags(price_stock)
+        publisher_info = soup.find('li', 'j-book_pub')
+        if publisher_info:
+            publisher = publisher_info.find('td', 'pubhouse')
+            publisher = publisher.text.strip()
+            publisher = self.remove_tags(publisher)
+            publisher = publisher.replace('\\n', '').strip()
 
-        cover = soup.findAll('div', 'j-book_autors book_autors')[0].findNextSibling('ul')
-        cover = cover.text.replace('\\n', '').strip()
-        cover = self.remove_tags(cover)
+        price_stock_info = soup.find('div', 'book_price3')
+        if price_stock_info:
+            price_stock = price_stock_info.find('div', 'book_price__title_ok-thin')
+            if price_stock:
+                price_stock = price_stock.text.replace('\\n', '').strip()
+                price_stock = self.remove_tags(price_stock)
+            else:
+                price_stock = u'В наличии'
+        else:
+            price_stock = u'В наличии'
 
-        row = {
-            "url": url,
-            "name": name,
-            "price": {
-                "currency": price_currency,
+        cover_info = soup.findAll('div', 'j-book_autors book_autors')
+        if cover_info:
+            cover_ul = cover_info[0].findNextSibling('ul')
+            if cover_ul:
+                cover = cover_ul.text.replace('\\n', '').strip()
+                cover = self.remove_tags(cover)
+            else:
+                cover = ''
+        else:
+            cover = ''
+
+        row = {}
+
+        row["url"] = url
+        row["name"] = name
+
+        row["availability"] = price_stock
+
+        if price:
+            row["price"] = {
+                "currency": "RUR",
                 "type": "currency",
                 "content": int(price)
-            },
-            "availability": price_stock,
-        }
-
+            }
 
         if author:
             row["author"] = author
@@ -116,10 +164,8 @@ class Parser(object):
         if images:
             row["images"] = images
 
-
         self.check_validate_schema(row)
         sout.write(json.dumps(row, ensure_ascii=False) + "\n")
-
 
     def check_validate_schema(self, node):
         f = open('books.schema.json', 'r')
