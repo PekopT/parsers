c69d2bb62194d3e89d27634d999cc970e201e023
Mukhtar <dagdahzub@mail.ru>
2016-04-21 22:26:24 +0300

    #PRS-51 [Finishes my-shop_ru #117632883]

--- book_store_html_parsers/my-shop_ru/parser.py
+++ book_store_html_parsers/my-shop_ru/parser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import traceback
 import json
@@ -7,7 +8,6 @@ from bs4 import BeautifulSoup
 from jsonschema import validate
 
 sout = getwriter("utf-8")(sys.stdout)
-
 
 
 class Parser(object):
@@ -36,27 +36,145 @@ class Parser(object):
         html = data['html']
         soup = BeautifulSoup(html, 'html.parser')
         name = soup.h1.text.strip()
-        description = u""
 
         stock = u"В наличии"
         row = {}
         row["url"] = url
         row["name"] = name
-        row["availability"] = stock
-        row["description"] = description
 
-        price_info = soup.find('td', 'w184 vat').find('table','w100p').find('b', {'style':'font-size:14px'})
-        price = re.sub(u'\D',u'', price_info.text)
+        author = ''
+        author_info = soup.find('td',text=re.compile(u"автор\/составитель"))
+        if author_info:
+            author = author_info.previous_sibling.text.strip()
 
-        row[u"price"] = {
+        description = ''
+        pictures = []
+        description_info = soup.find('td', {'data-o': 'good_description'})
+        if description_info:
+            description_info = description_info.select("table tr")[2]
+            if description_info:
+                images_info = description_info.find_all('img')
+                if images_info:
+                    pictures = [img.get('src') for img in images_info]
+                description = description_info.text.strip()
+
+        div_small = soup.find('div', )
+
+        year = ''
+        year_info = soup.find('', text=re.compile(ur'дата\sвыпуска', re.DOTALL))
+        if year_info:
+            year = year_info
+            year = re.sub('\D', '', year)
+
+        isbn = ''
+        isbn_info = soup.find('span', text=re.compile(u"ISBN"))
+        if isbn_info:
+            isbn = isbn_info.text.strip()
+            isbn = isbn.replace(u"ISBN:","").strip()
+
+
+        cover = ''
+        cover_info = soup.find('', text=re.compile(u"переплет"))
+        if cover_info:
+            cover = cover_info.strip()
+
+        publisher = ''
+        publisher_info = soup.find('',text=re.compile(u"Издательство"))
+        if publisher_info:
+            publisher = publisher_info.next_sibling.text.strip()
+
+        pages = ''
+        pages_info = soup.find('',text=re.compile(u"количество\sстраниц"))
+        if pages_info:
+            pages = pages_info.strip()
+            pages = re.sub('\D','', pages)
+
+
+        price = ''
+        price_info = soup.find('td','bgcolor_2 list_border')
+        if price_info:
+            price_info = price_info.find('b')
+            if price_info:
+                price = price_info.text.strip()
+                price = re.sub('\D', '', price)
+
+        # price_info = soup.find('td', 'w184 vat').find('table','w100p').find('b', {'style':'font-size:14px'})
+        # price = re.sub(u'\D',u'', price_info.text)
+
+
+        also_buy_info = soup.find('table',{'data-o':'showcase_complementary'})
+        if also_buy_info:
+            also_buy_info_td = also_buy_info.find_all('tr')[1].find_all('td')
+        else:
+            also_buy_info_td = []
+
+
+        also_buy_books = []
+
+        for book in also_buy_info_td:
+            picture_book = book.find('img').get('src')
+            name_book = book.find('span', 'small1').text.strip()
+            price_book = book.find('div','small1').text.strip()
+            price_book = re.sub('\D','', price_book)
+            url_book = book.a.get('href')
+
+            also_row = {
+                "url": url_book,
+                "name": name_book,
+                "image": picture_book,
+                "price": {
+                    "currency": "RUR",
+                    "type": "currency",
+                    "content": int(price_book),
+                }
+            }
+
+            also_buy_books.append(also_row)
+
+        row = {
+            "url": url,
+            "name": name
+        }
+
+        if price:
+            row["price"] = {
                 "currency": "RUR",
                 "type": "currency",
-                "content": price
-        }
+                "content": int(price)
+            }
+
+        if author:
+            row["author"] = author
+
+        if publisher:
+            row["publisher"] = publisher
+
+        if description:
+            row["description"] = description
+
+        if year:
+            row["year"] = year
+
+        if pages:
+            row["pages"] = pages
+
+        if isbn:
+            row["isbn"] = isbn
+
+        if stock:
+            row["availability"] = stock
+
+        if cover:
+            row["cover"] = cover
+
+        if pictures:
+            row["images"] = pictures
+
+        if also_buy_books:
+            row["also_buy"] = also_buy_books
 
         self.check_validate_schema(row)
         sout.write(json.dumps(row, ensure_ascii=False) + "\n")
-        # self.rows_data.append(row)
 
     def check_validate_schema(self, node):
         f = open('books.schema.json', 'r')
@@ -65,7 +183,6 @@ class Parser(object):
 
     def close_parser(self):
         pass
-        # sout.write(json.dumps(self.rows_data, ensure_ascii=False))
 
 
 def main():
