f76da57a75fcb8ef976899c37d4e3b4b6f2194f6
Mukhtar <dagdahzub@mail.ru>
2016-04-21 18:14:19 +0300

    #PRS-44 [Finishes #booka_ru #117631737]

--- book_store_html_parsers/booka_ru/parser.py
+++ book_store_html_parsers/booka_ru/parser.py
@@ -72,10 +72,6 @@ class Parser(object):
             else:
                 price = ''
 
-
-
-
-
         row = {
             "url": url,
             "name": name
@@ -111,7 +107,6 @@ class Parser(object):
 
         self.check_validate_schema(row)
         sout.write(json.dumps(row, ensure_ascii=False) + "\n")
-        # self.rows_data.append(row)
 
     def check_validate_schema(self, node):
         f = open('books.schema.json', 'r')
@@ -120,7 +115,6 @@ class Parser(object):
 
     def close_parser(self):
         pass
-        # sout.write(json.dumps(self.rows_data, ensure_ascii=False))
 
 
 def main():
